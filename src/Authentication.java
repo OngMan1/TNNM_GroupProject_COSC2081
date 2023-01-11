e6ec3caad9aeba52a675b38e5a096c3e9462894e
OngMan1 <102709104+OngMan1@users.noreply.github.com>
2023-01-11 19:58:47 +0700

    change name

--- src/Authentication.java
+++ src/Authentication.java
@@ -1,10 +1,10 @@
 class Authentication {
-    private static final String USER = "user.txt";
-    private static final String ADMIN = "admin.txt";
+    private static final String USER_DETAILS = "user_details.txt";
+    private static final String ADMIN_DETAILS = "admin_details.txt";
 
     public static String getUserFile(Admin admin) {
         if (admin != null) {
-            return USER;
+            return USER_DETAILS;
         }
         return null;
     }
@@ -17,9 +17,9 @@ class Authentication {
     private String[] logging_in(String type, String username, String password) {
         switch (type) {
             case "customer":
-                return search(USER, username, password);
+                return search(USER_DETAILS, username, password);
             case "admin":
-                return search(ADMIN, username, password);
+                return search(ADMIN_DETAILS, username, password);
             default:
                 System.out.println("Err: Wrong login type");
                 return null;
